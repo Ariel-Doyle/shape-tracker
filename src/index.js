3ef42728088a83bbb640b9721014093de91d433e
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-12-11 16:04:47 -0800

    run first passing tet in triangle.js using jest.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Triangle from './triangle.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../css/styles.css';
+import './css/styles.css';
 
 function handleTriangleForm(e) {
   e.preventDefault();
